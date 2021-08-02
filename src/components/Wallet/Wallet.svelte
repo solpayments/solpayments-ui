96a08f66f4c4ff7fe660eeb13c7f2319cbe707d8
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-08-02 21:32:12 +0300

    Update Wallet to work with HTML theme

--- src/components/Wallet/Wallet.svelte
+++ src/components/Wallet/Wallet.svelte
@@ -2,34 +2,45 @@
   import { adapter, connected } from '../../stores';
   import { connectToWallet } from '../../helpers/wallet';
 
+  const afterAction = () => {
+    const modalButton = document.getElementById('connectModalButton');
+    if (modalButton) {
+      modalButton.click();
+    }
+  };
+
   let promise: Promise<void> | null;
   const handleConnect = () => {
     promise = connectToWallet().then(() => {
       promise = null;
+      afterAction();
     });
   };
   const handleDisconnect = () => {
     promise = null;
     adapter.update(() => undefined);
+    afterAction();
   };
 </script>
 
-<main>
-  {#if !$connected}
-    <button on:click={() => handleConnect()}> Connect </button>
-  {/if}
+{#if !$connected}
+  <button class="btn btn-outline-primary" type="button" on:click={() => handleConnect()}>
+    Connect
+  </button>
+{/if}
 
-  {#if !$connected && promise != null}
-    {#await promise}
-      <p>Connecting</p>
-    {:then _pubkey}
-      <p style="color: green">Connected to {$adapter && $adapter.publicKey}</p>
-    {:catch error}
-      <p style="color: red">{error}</p>
-    {/await}
-  {/if}
+{#if !$connected && promise != null}
+  {#await promise}
+    <!-- connecting -->
+  {:then _pubkey}
+    <!-- Connected to {$adapter && $adapter.publicKey} -->
+  {:catch error}
+    <!-- {error} -->
+  {/await}
+{/if}
 
-  {#if $connected}
-    <button on:click={() => handleDisconnect()}> Disconnect </button>
-  {/if}
-</main>
+{#if $connected}
+  <button class="btn btn-outline-primary" type="button" on:click={() => handleDisconnect()}>
+    Disconnect
+  </button>
+{/if}
