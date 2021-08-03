fb76554075a42df5f0116950f69c19d80bf832f9
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-08-03 23:05:50 +0300

    Use provided committment + get correct pubkey

--- src/solpayments/merchants.ts
+++ src/solpayments/merchants.ts
@@ -3,7 +3,6 @@ import type { Connection } from '@solana/web3.js';
 import type { Result } from '../helpers/result';
 import { failure, success } from '../helpers/result';
 import { getUiAmount } from '../helpers/utils';
-import { CONFIRMED } from '../helpers/constants';
 import { MERCHANT_ACC_OWNER_FIELD_OFFSET, SOL_DECIMALS } from './constants';
 import { MERCHANT_LAYOUT } from './layout';
 import type { Merchant } from './layout';
@@ -26,7 +25,7 @@ export const getMerchantAccounts = async (
 
   try {
     const result = await connection.getProgramAccounts(programIdKey, {
-      commitment: CONFIRMED,
+      commitment: connection.commitment,
       filters: [
         { memcmp: { offset: MERCHANT_ACC_OWNER_FIELD_OFFSET, bytes: ownerKey.toBase58() } },
       ],
@@ -38,7 +37,7 @@ export const getMerchantAccounts = async (
       result.map((item) => {
         const merchantData = MERCHANT_LAYOUT.decode(item.account.data);
         return {
-          address: result[0].pubkey,
+          address: item.pubkey,
           account: {
             ...merchantData,
             fee: getUiAmount(merchantData.fee, SOL_DECIMALS),
