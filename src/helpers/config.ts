2af6614771edcefcfe49bcf3f43d0b0e8b35e093
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-08-06 08:27:38 +0300

    Set EXPLORER_URL

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,2 +1,3 @@
 export const RPC_API_URL = process.env.SVELTE_APP_RPC_API_URL || 'https://api.devnet.solana.com';
+export const EXPLORER_URL = process.env.SVELTE_APP_EXPLORER_URL || 'https://explorer.solana.com';
 export const PROGRAM_ID = process.env.SVELTE_APP_PROGRAM_ID || '';
